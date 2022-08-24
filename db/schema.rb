45f10b0398a01d124c672cc8a8ea6f997911133a
Damian Demasi <damian.demasi.1@gmail.com>
2022-08-25 08:31:46 +0930

    Migration for Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_222044) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_225903) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
