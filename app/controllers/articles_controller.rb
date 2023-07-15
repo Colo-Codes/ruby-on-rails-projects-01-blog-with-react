a0ea52008b1d26f9b8935958670dd83b8d13f831
Damian Demasi <damian.demasi.1@gmail.com>
2023-07-15 16:59:12 +0930

    Articles creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,21 @@ class ArticlesController < ApplicationController
   def new; end
 
   def create
-    render plain: params[:article].inspect
+    # Create a new model instance using the params hash
+    # params[:article]: "article"=>{"title"=>"Title of Article", "body"=>"Body text"}
+    @article = Article.new(article_params)
+
+    # Save the model in the database
+    @article.save
+
+    # Redirect the user to the show action
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    # params[:article]: "article"=>{"title"=>"Title of Article", "body"=>"Body text"}
+    params.require(:article).permit(:title, :body)
   end
 end
